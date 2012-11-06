48728570033ed4c311c5af6622ecdb730158d173
jieshan <jshan@macalester.edu>
2012-11-05 22:36:46 -0600

    gitignore

--- MyCafeMac/src/razzleDazzlers/mycafemac/MenuVegeActivity.java
+++ MyCafeMac/src/razzleDazzlers/mycafemac/MenuVegeActivity.java
@@ -1,14 +1,48 @@
 package razzleDazzlers.mycafemac;
 
-import razzleDazzlers.ratecafemac.R;
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
 
-public class MenuVegeActivity extends Activity {
+import razzleDazzlers.ratecafemac.R;
+import razzleDazzlers.util.DishArrayAdapter;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MenuVegeActivity extends ListActivity {
 	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menuvege);
+       
+        ArrayList<String> vegeMenu = getIntent().getStringArrayListExtra("vegeMenu");
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> des = new ArrayList<String>();
+        for(int i=0;i<vegeMenu.size();i++){
+			if(i%2 == 0){
+				names.add(vegeMenu.get(i));
+			}else{
+				des.add(vegeMenu.get(i));
+			}
+		}
+		
+		setListAdapter(new DishArrayAdapter(this, names, des));
     }
+    
+	@Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+
+		System.out.println("*****");
+		//get selected items
+		TextView textView = (TextView) v.findViewById(R.id.name);
+		String text = textView.getText().toString();
+		System.out.println(text);
+		//String selectedValue = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+	}
     
 }
