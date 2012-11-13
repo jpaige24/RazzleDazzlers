3a9846125174d943739db4ef1291687371f36820
Yu Zhao <yzhao1@macalester.edu>
2012-11-13 14:50:27 -0600

    Initial Adding Test Package

--- MyCafeMac/src/razzleDazzlers/test/test.java
+++ MyCafeMac/src/razzleDazzlers/test/test.java
@@ -7,23 +7,74 @@ import org.junit.Test;
 import razzleDazzlers.util.Server;
 
 public class test {
+	
+	private static final double DELTA = 1e-15;
+	Server tester = new Server();
+	String name1 = "Dish1";
+	String name2 = "Dish2";
+	String date1 = "11132012";
+	String date2 = "11142012";
+	String userID1 = "000000000";
+	String userID2 = "000000001";
+	
+	@Test
+	public void testInsert() {
+		
+		/*testing insert*/
+		tester.insert(userID1, date1, name1, (float) 3.5);
+		tester.insert(userID2, date1, name1, (float) 4.5);
+		tester.insert(userID1, date1, name2, (float) 4.0);
+		tester.insert(userID2, date2, name1, (float) 4.0);		
+		tester.insert(userID1, date2, name2, (float) 3.5);
+		tester.insert(userID2, date2, name2, (float) 4.5);
+		
+	}
 
 	@Test
 	public void testGetAvgDishRating() {
-		Server tester = new Server();
-		String name = "Something not tasty";
+
+		/*testing getAvgDishRating*/
+		assertEquals((float) 4, tester.getAvgDishRating(name1, date1), DELTA);
+		assertEquals((float) 4.5, tester.getAvgDishRating(name1, date2), DELTA);
+				
+	}
+	
+	@Test
+	public void testUserDishRating() {
 		
+		/*testing getUserDishRating*/
+		assertEquals((float) 3.5, tester.getUserDishRating(name1, date1, userID1), DELTA);
+		assertEquals((float) 4.5, tester.getUserDishRating(name2, date1, userID1), DELTA);
 		
-		String date = "11132012";
-		
-		
-		String userID1 = "000000000";
-		String userID2 = "000000001";
-		
-		tester.insert(userID1, date, name, (float) 3.5);
-		tester.insert(userID2, date, name, (float) 3.5);
-		assertEquals("Result", tester.getAvgDishRating(name, date));
 		fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	public void testDayRating() {
+		
+		/*testing getDayRating*/
+		assertEquals((float) 4, tester.getDayRating(date1), DELTA);
+		assertEquals((float) 4, tester.getDayRating(date2), DELTA);	
+		
+	}
+	
+	public void testUpdate() {
+		
+		/*testing update*/
+		tester.update(userID1, date1, name1, (float) 4.0);
+		tester.update(userID2, date1, name1, (float) 4.0);
+		assertEquals((float) 4.0, tester.getDayRating(date1), DELTA);
+	
+	}
+	
+	public void testCheck() {
+		
+		/*testing Check*/
+		assertEquals(true,tester.check(userID1, date1, name1));
+		assertEquals(true,tester.check(userID2, date1, name1));
+		assertEquals(false,tester.check(userID1, date2, name1));
+		
 	}
 
 }
